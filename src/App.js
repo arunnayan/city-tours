033562ffd905361b9f89456d059ec102dccfece5
Arun Singh <arunnayan.28@gmail.com>
2019-06-08 15:28:15 -0700

    Adding Navbar page

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
-import React from "react";
+import React, { Component } from "react";
 import "./App.scss";
-
-function App() {
-  return <h1>Hi</h1>;
+import Navbar from "./components/Navbar/Navbar";
+class App extends Component {
+  render() {
+    return (
+      <React.Fragment>
+        <Navbar />
+      </React.Fragment>
+    );
+  }
 }
 
 export default App;
